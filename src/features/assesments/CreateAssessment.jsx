44e2d515e22f738dfe7a6c271a1857e7966d5d39
segni <segni.836@gmail.com>
2025-04-28 10:16:20 +0300

    remove mui

--- src/features/assesments/CreateAssessment.jsx
+++ src/features/assesments/CreateAssessment.jsx
@@ -6,28 +6,8 @@ import Button from '../../components/Button'
 import QuestionPreview from '../../components/QuestionPreview'
 import QuestionModal from '../../components/QuestionModal'
 import TextField from '@mui/material/TextField';
-import { alpha, styled } from '@mui/material/styles';
 const CreateAssessment = () => {
-  const CssTextField = styled(TextField)({
-    '& label.Mui-focused': {
-      color: '#A0AAB4',
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#B2BAC2',
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#E0E3E7',
-      },
-      '&:hover fieldset': {
-        borderColor: '#B2BAC2',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#6F7E8C',
-      },
-    },
-  });
-  
+
   // const [selectedAnswers, setSelectedAnswers] = useState({});
   const [selectedQuestions, setSelectedQuestions] = useState([]);
   const handleSectionSelect = (questions) => {
