04fa0680b52b4cada1d6a3ad44d6a56f377c1b93
segni <segni.836@gmail.com>
2025-06-15 23:27:39 +0300

    fix Logo position

--- src/features/auth/AuthLayout.jsx
+++ src/features/auth/AuthLayout.jsx
@@ -13,7 +13,7 @@ const AuthLayout = () => {
             <img
               src={logo}
               alt="logo"
-              className="absolute rounded-full inset-0 w-10 h-10 max-w-24 max-h-24 top-7 left-7"
+              className="absolute rounded-full inset-0 w-10 h-10 max-w-24 max-h-24 md:top-7 md:left-7  top-1 left-2"
             />
           </div>
           <Outlet />
